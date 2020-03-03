00c1a2a13f0b67123159377c78bc39be1b163844
Fabrice Creuzot <code@luigifab.fr>
2020-03-03 20:00:00 +0200

    Version 2.3.1

--- app/code/community/Luigifab/Paypalrefund/Block/Adminhtml/Config/Help.php
+++ app/code/community/Luigifab/Paypalrefund/Block/Adminhtml/Config/Help.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created V/05/06/2015
- * Updated J/26/09/2019
+ * Updated S/01/02/2020
  *
  * Copyright 2015-2020 | Fabrice Creuzot (luigifab) <code~luigifab~fr>
  * https://www.luigifab.fr/magento/paypalrefund
@@ -23,28 +23,32 @@ class Luigifab_Paypalrefund_Block_Adminhtml_Config_Help extends Mage_Adminhtml_B
 
 		$msg = $this->checkRewrites();
 		if ($msg !== true)
-			return sprintf('<p class="box">%s %s <span style="float:right;"><a href="https://www.%s">%3$s</a> | ⚠ IPv6</span></p>'.
+			return sprintf('<p class="box">%s %s <span class="right"><a href="https://www.%s">%3$s</a> | ⚠ IPv6</span></p>'.
 				'<p class="box" style="margin-top:-5px; color:white; background-color:#E60000;"><strong>%s</strong><br />%s</p>',
 				'Luigifab/Paypalrefund', $this->helper('paypalrefund')->getVersion(), 'luigifab.fr/magento/paypalrefund',
 				$this->__('INCOMPLETE MODULE INSTALLATION'),
 				$this->__('There is conflict (<em>%s</em>).', $msg));
 
-		return sprintf('<p class="box">%s %s <span style="float:right;"><a href="https://www.%s">%3$s</a> | ⚠ IPv6</span></p>',
+		return sprintf('<p class="box">%s %s <span class="right"><a href="https://www.%s">%3$s</a> | ⚠ IPv6</span></p>',
 			'Luigifab/Paypalrefund', $this->helper('paypalrefund')->getVersion(), 'luigifab.fr/magento/paypalrefund');
 	}
 
 	private function checkRewrites() {
 
 		$rewrites = [
-			['block', 'paypal/standard_redirect'],
-			['model', 'paypal/standard']
+			['block' => 'paypal/standard_redirect'],
+			['model' => 'paypal/standard']
 		];
 
 		foreach ($rewrites as $rewrite) {
-			if (($rewrite[0] == 'model') && (mb_stripos(get_class(Mage::getModel($rewrite[1])), 'luigifab') === false))
-				return $rewrite[1];
-			else if (($rewrite[0] == 'block') && (mb_stripos(get_class(Mage::getBlockSingleton($rewrite[1])), 'luigifab') === false))
-				return $rewrite[1];
+			foreach ($rewrite as $type => $class) {
+				if (($type == 'model') && (mb_stripos(Mage::getConfig()->getModelClassName($class), 'luigifab') === false))
+					return $class;
+				else if (($type == 'block') && (mb_stripos(Mage::getConfig()->getBlockClassName($class), 'luigifab') === false))
+					return $class;
+				else if (($type == 'helper') && (mb_stripos(Mage::getConfig()->getHelperClassName($class), 'luigifab') === false))
+					return $class;
+			}
 		}
 
 		return true;
